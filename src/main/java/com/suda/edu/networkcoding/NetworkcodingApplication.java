8671bb1da9f3f8de1d0f4c0b234d13a570048216
axin <593294489@qq.com>
2018-12-26 17:19:07 +0800

    Initial commit

---
+++ src/main/java/com/suda/edu/networkcoding/NetworkcodingApplication.java
@@ -0,0 +1,14 @@
+package com.suda.edu.networkcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkcodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkcodingApplication.class, args);
+    }
+
+}
+
